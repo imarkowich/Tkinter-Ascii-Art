15e4aa1af37c10ed2a21e3fc432c49708c37a21b
Ian Markowich <imarkowich223@gmail.com>
2022-06-24 13:01:44 -0400

    adds root window

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from tkinter import ttk
 # from art import *
 
 # make root
+root = tk.Tk()
+root.title('Tkinter Ascii Art')
+root.iconbitmap("./assets/duck.ico")
+root.resizable(0, 0)
+
+# root geometry
+window_width = 700
+window_height = 500
+y_offset = 50
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+center_x = int(screen_width / 2 - window_width / 2)
+center_y = int(screen_height / 2 - window_height / 2) - y_offset
+
+root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
 # make labels - title and directions
 
@@ -18,3 +35,5 @@ from tkinter import ttk
 # make Button - clear
 
 # make Button - info
+
+root.mainloop()
