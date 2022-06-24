3097575600ca20c3fe89932830ce5f75a495d487
Ian Markowich <imarkowich223@gmail.com>
2022-06-24 12:56:11 -0400

    adds asset folder & main file

---
+++ main.py
@@ -0,0 +1,20 @@
+# Tkinter Ascii Art
+import tkinter as tk
+from tkinter import ttk
+# from art import *
+
+# make root
+
+# make labels - title and directions
+
+# make Entry - input text
+
+# make Listbox/Scrollbar - font choose
+
+# make Text - ascii output
+
+# make Button - copy
+
+# make Button - clear
+
+# make Button - info
